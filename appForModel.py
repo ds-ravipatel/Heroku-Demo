bcfd4d8f7c6e5886c4df1f71528f0241b35b7881
ds-ravipatel <54357618+ds-ravipatel@users.noreply.github.com>
2019-08-31 12:15:19 +0530

    Add files via upload

--- appForModel.py
+++ appForModel.py
@@ -11,17 +11,21 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
+    #using below - parameters will be stored in list
     int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
+    #below is to convert single parameter to integer. DO not use if multiple parameters
+    res = sum(d * 10**i for i, d in enumerate(int_features[::-1]))
+    input = np.array([1])
+    featsqr = res*res
+    featcube = res*res*res
+    input = np.append(input,res)
+    input = np.append(input,featsqr)
+    input = np.append(input,featcube)
+    #convert 1D array to 2D array.
+    input = input.reshape(1,-1)
+    prediction = model.predict(input)
     output = round(prediction[0], 2)
-
     return render_template('index.html', prediction_text='Salary at this level should be $ {}'.format(output))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
